25e2795b0aacc8999c80e2b55f272c2cb46f1699
cow0731 <cow0731@gmail.com>
2024-11-03 14:46:20 +0900

    edit .gitignore file

---
+++ src/main/java/com/pado/kidneybeanback/KidneyBeanBackApplication.java
@@ -0,0 +1,13 @@
+package com.pado.kidneybeanback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidneyBeanBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidneyBeanBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pado/kidneybeanback/KidneyBeanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pado.kidneybeanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidneyBeanBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
